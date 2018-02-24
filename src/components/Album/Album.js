3f43ff279de07575dbf3b66ec646d736309d0267
menor <josemenor@gmail.com>
2018-02-24 08:53:29 +0100

    Add storybook, remove some create-react-app scafold

---
+++ src/components/Album/Album.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Album = () => {
+  return <div />;
+};
+
+export default Album;
---
+++ src/components/Album/Album.stories.js
@@ -0,0 +1,6 @@
+import React from 'react';
+
+import { storiesOf } from '@storybook/react';
+import Album from '.';
+
+storiesOf('Album', module).add('Default', () => <Album />);
