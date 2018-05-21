ea0fe32f1e0d366e79dc379369a8336ccfe0ee97
menor <josemenor@gmail.com>
2018-05-21 20:20:52 +0200

    Remove storybook

--- .storybook/config.js
+++
@@ -1,9 +0,0 @@
-import { configure } from '@storybook/react';
-
-const components = require.context('../src/components', true, /.stories.js$/);
-
-function loadStories() {
-  components.keys().forEach(filename => components(filename));
-}
-
-configure(loadStories, module);
